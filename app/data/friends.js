d185153ee9176b52b9d802e8107f41159cf2e759
Alex Green <agreen@santa-ana.org>
2018-03-21 17:46:02 -0700

    Completed HTML routes and API route to send friends JSON data.

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+var friends = [
+    {
+        name: 'Kal-el',
+        photo: '#',
+        scores: [
+            '10',
+            '9',
+            '8',
+            '7',
+            '6',
+            '5',
+            '4',
+            '3',
+            '2',
+            '1'
+        ]
+    },{
+        name: 'Bruce Wayne',
+        photo: '#',
+        scores: [
+            '9',
+            '8',
+            '7',
+            '6',
+            '5',
+            '4',
+            '3',
+            '2',
+            '1',
+            '10'
+        ]
+    }
+];
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express(); 
+
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var PORT = process.env.PORT || 3000;
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+ 
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+ 
+// parse various different custom JSON types as JSON
+app.use(bodyParser.json({ type: 'application/*+json' }))
+ 
+// parse some custom thing into a Buffer
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+ 
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }))
+
+require('./app/routing/api-routes.js')(app);
+require('./app/routing/html-routes.js')(app);
+
+app.listen(PORT, function() {
+    console.log("Serving running on port: " + PORT);
+})
